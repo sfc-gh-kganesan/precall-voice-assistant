90c79e72fd4df4522e966980774b367a9b878c9c
Jason Summer <jason.summer@snowflake.com>
2025-10-17 13:16:24 -0500

    [SANDBOX] fix: Uncomment script (#80)

--- sandbox/jsummer/langgraph-service-function/release/deploy.sh
+++ sandbox/jsummer/langgraph-service-function/release/deploy.sh
@@ -5,28 +5,28 @@ IMAGE=langgraph-service-function
 TAG=latest
 SERVICE_NAME=langgraph
 
-# echo "Creating database if not exists"
-# snow object create database $DATABASE --if-not-exists
+echo "Creating database if not exists"
+snow object create database $DATABASE --if-not-exists
 
-# echo "Creating schema if not exists"
-# snow object create schema $SCHEMA --database $DATABASE --if-not-exists
+echo "Creating schema if not exists"
+snow object create schema $SCHEMA --database $DATABASE --if-not-exists
 
-# echo "Creating compute pool"
-# snow spcs compute-pool deploy $SNOW_CONNECT || true
+echo "Creating compute pool"
+snow spcs compute-pool deploy $SNOW_CONNECT || true
 
-# echo "Creating image repository"
-# snow spcs image-repository deploy $SNOW_CONNECT --database $DATABASE --schema $SCHEMA || true
+echo "Creating image repository"
+snow spcs image-repository deploy $SNOW_CONNECT --database $DATABASE --schema $SCHEMA || true
 
-# echo "Logging in to image registry"
-# snow spcs image-registry login $SNOW_CONNECT
+echo "Logging in to image registry"
+snow spcs image-registry login $SNOW_CONNECT
 
-# echo "Building image"
-# docker build --rm --platform linux/amd64 -t $REPO/$IMAGE:$TAG ../.
+echo "Building image"
+docker build --rm --platform linux/amd64 -t $REPO/$IMAGE:$TAG ../.
 
-# echo "Pushing image to repository"
-# docker push $REPO/$IMAGE:$TAG
+echo "Pushing image to repository"
+docker push $REPO/$IMAGE:$TAG
 
-# echo "Dropping service if exists"
+echo "Dropping service if exists"
 snow spcs service drop $SERVICE_NAME $SNOW_CONNECT --database $DATABASE --schema $SCHEMA || true
 
 echo "Deploying service"
