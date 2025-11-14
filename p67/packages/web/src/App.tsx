6173017161b77dcb366f7f8984081ec7aeb079c4
Seth Li <135057755+sfc-gh-setli@users.noreply.github.com>
2025-11-14 15:51:42 -0800

    chore(p67): Add .editorconfig and format:check pnpm target (#212)
    - pnpm target: rename `type-check` to `type:check`
    - pnpm target: add `format:check` (will be used in CI to ensure incorrectly formatted files don't get checked in)
    - fix formatting by running `format:fix`
    - add `.editorconfig` file in project root directory

--- p67/packages/web/src/App.tsx
+++ p67/packages/web/src/App.tsx
@@ -2,18 +2,20 @@ import { SingleLine, Heading, Paragraph, Layout } from '@snowflake/stellar-compo
 import { AgentIcon } from '@snowflake/stellar-icons';
 
 function App() {
-    return (
-        <Layout.Root style={{ margin: 100 }}>
-            <Layout.Header>
-                <SingleLine bold>
-                    <Heading size='pageHeader'><AgentIcon /> p67</Heading>
-                </SingleLine>
-            </Layout.Header>
-            <Layout.Content>
-                <Paragraph>Agent Builder</Paragraph>
-            </Layout.Content>
-        </Layout.Root>
-    );
+  return (
+    <Layout.Root style={{ margin: 100 }}>
+      <Layout.Header>
+        <SingleLine bold>
+          <Heading size="pageHeader">
+            <AgentIcon /> p67
+          </Heading>
+        </SingleLine>
+      </Layout.Header>
+      <Layout.Content>
+        <Paragraph>Agent Builder</Paragraph>
+      </Layout.Content>
+    </Layout.Root>
+  );
 }
 
 export default App;
